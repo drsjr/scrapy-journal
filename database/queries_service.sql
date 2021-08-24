8e106dbf46c9295518d3ee40669737b31d814d70
Junior <djalma.rsjr@gmail.com>
2021-08-24 03:03:24 -0300

    change query cansada

--- database/queries_service.sql
+++ database/queries_service.sql
@@ -19,41 +19,59 @@ SELECT * FROM news_main, news_column, news_carrossel
 
 SELECT * FROM article;
 
-SELECT
-    'main' AS is_from,
-    ar.id,
-    ar.url,
-    ar.title,
-    ar.subtitle,
-    ar.image,
-    ar.category_id
-FROM front_page fp
-INNER JOIN news_main n ON n.front_page_id = fp.id
-INNER JOIN article ar ON n.article_id = ar.id
-UNION
 SELECT 
-    'carrossel' AS is_from,
-    ar.id,
-    ar.url,
-    ar.title,
-    ar.subtitle,
-    ar.image,
-    ar.category_id
-FROM front_page fp
-INNER JOIN news_carrossel n ON n.front_page_id = fp.id
-INNER JOIN article ar ON n.article_id = ar.id
-UNION
-SELECT 
-    'column' AS is_from,
-    ar.id,
-    ar.url,
-    ar.title,
-    ar.subtitle,
-    ar.image,
-    ar.category_id
-FROM front_page fp
-INNER JOIN news_column n ON n.front_page_id = fp.id
-INNER JOIN article ar ON n.article_id = ar.id;
+    t.is_from,
+    t.id,
+    t.url,
+    t.title,
+    t.subtitle,
+    t.image,
+    t.category_id,
+    t.created_at,
+    t.front_page_id
+FROM (
+    SELECT
+        'main' AS is_from,
+        ar.id,
+        ar.url,
+        ar.title,
+        ar.subtitle,
+        ar.image,
+        ar.category_id,
+        ar.created_at,
+        fp.id AS front_page_id
+    FROM front_page fp
+    INNER JOIN news_main n ON n.front_page_id = fp.id
+    INNER JOIN article ar ON n.article_id = ar.id
+    UNION
+    SELECT 
+        'carrossel' AS is_from,
+        ar.id,
+        ar.url,
+        ar.title,
+        ar.subtitle,
+        ar.image,
+        ar.category_id,
+        ar.created_at,
+        fp.id AS front_page_id
+    FROM front_page fp
+    INNER JOIN news_carrossel n ON n.front_page_id = fp.id
+    INNER JOIN article ar ON n.article_id = ar.id
+    UNION
+    SELECT 
+        'column' AS is_from,
+        ar.id,
+        ar.url,
+        ar.title,
+        ar.subtitle,
+        ar.image,
+        ar.category_id,
+        ar.created_at,
+        fp.id AS front_page_id
+    FROM front_page fp
+    INNER JOIN news_column n ON n.front_page_id = fp.id
+    INNER JOIN article ar ON n.article_id = ar.id) AS t
+WHERE t.front_page_id = 9;
 
 
 
