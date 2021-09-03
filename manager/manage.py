b22a8b763073ecc42dd2cd55873604142e1da7a0
Junior <djalma.rsjr@gmail.com>
2021-09-03 02:54:07 -0300

    use requests

--- manager/manage.py
+++ manager/manage.py
@@ -1,6 +1,8 @@
 import os
+from typing import List
 import psycopg2
 import json
+import requests
 
 POSTGRES_HOSTNAME = "172.17.0.2"
 POSTGRES_PORT = 5432
@@ -16,26 +18,48 @@ connection = psycopg2.connect(
             dbname=POSTGRES_DBNAME,
             port=POSTGRES_PORT)
 
+
+LIST_JOBS = 'http://localhost:6800/listjobs.json?project=journal'
+
 category_spider_api_call = 'curl http://192.168.15.35:6800/schedule.json -d project=journal -d spider=categories -d category={0}'
 principal_spider_api_call = 'curl http://192.168.15.35:6800/schedule.json -d project=journal -d spider=principal'
 
-categories = [ 'ultimas', 'jundiai', 'opiniao', 'politica', 'economia', 'policia', 'esportes', 'cultura', 'hype']
+#categories = ["ultimas", "jundiai", "opiniao", "politica", "economia", "policia", "esportes", "cultura", "hype"]
 
 category_spider_api_call_local = 'curl http://localhost:6800/schedule.json -d project=journal -d spider=categories -d category={0}'
 front_page_spider_api_call_local = 'curl http://localhost:6800/schedule.json -d project=journal -d spider=frontpage'
-principal_spider_api_call_local = 'curl http://localhost:6800/schedule.json -d project=journal -d spider=principal'
 article_spider_api_call_local = 'curl http://localhost:6800/schedule.json -d project=journal -d spider=article -d path={0}'
 
 
-def call_all_categories():
-    for c in categories:
-        os.system(category_spider_api_call_local.format(c))
+def list_jobs():
+    """
+        curl http://localhost:6800/listjobs.json?project=journal
 
-def call_principal():
-    os.system(principal_spider_api_call_local)
+    """
+    list_jobs_request = requests.get(LIST_JOBS)
+    value = list_jobs_request.json()
+    print(value)
+
+
+def call_all_categories(categories: List[str]):
+    """
+        curl http://localhost:6800/schedule.json -d project=journal -d spider=categories -d category={0}
+
+    """
+    for c in categories:
+        payload = [('project', 'journal'), ('spider', 'categories'), ('category', c)]
+        list_jobs_request = requests.post("http://localhost:6800/schedule.json", data=payload)
+        print(list_jobs_request.json())
+
 
 def call_front_page():
-    os.system(front_page_spider_api_call_local)
+    """
+        curl http://localhost:6800/schedule.json -d project=journal -d spider=frontpage
+
+    """
+    payload = [('project', 'journal'), ('spider', 'frontpage')]
+    list_jobs_request = requests.post("http://localhost:6800/schedule.json", data=payload)
+    return list_jobs_request.json()
 
 
 def call_article_from_front_page():
